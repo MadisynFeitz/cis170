14fc8356610d04f48c8e194f7a4469553072bd45
Madisyn Feitz <sMadisynFeitz@yccc.edu>
2018-09-05 19:08:43 -0400

    first commit

---
+++ src/myFirstProject/HelloWorld.java
@@ -0,0 +1,20 @@
+package myFirstProject;
+
+public class HelloWorld 
+{
+	
+	//An object is 2 things.
+	//1. Actions. Methods.
+	//2. Attributes. Variables.
+	private String name = "Mike";
+	
+	// main method. Starts the program.
+	//Method Signature - What makes this method unique. Defines the method.
+	public static void main(String[] args) 
+	{
+	// Prints to the console.	
+		System.out.println("Hello World");
+		System.out.println("My First Java App!!!");
+	}
+
+}
